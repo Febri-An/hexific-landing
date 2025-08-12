66f1d6bf1809db78f1a64b2f2af4a5a02ba6c5aa
Febri Nirwana <febri.anjy.nirwana.354@gmail.com>
2025-08-12 20:11:16 +0700

    feat(seo): add sitemap generation and integrate with robots.txt

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://hexific.com', // ganti sama domain lu
+  generateRobotsTxt: true, // bikin robots.txt juga
+  sitemapSize: 7000, // optional
+}
