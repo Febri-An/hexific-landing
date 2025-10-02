6b076f5fca087efe6ed78f9b59e9d8df5c8a2754
Febri Nirwana <febri.anjy.nirwana.354@gmail.com>
2025-10-02 17:11:41 +0700

    fix: replace incorrect Hexific logo in footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -803,9 +803,10 @@ export default function Page() {
             <div>
               <div className="flex items-center space-x-3 mb-4">
                 <div className="w-8 h-8 bg-lime-400 rounded-lg flex items-center justify-center">
-                  <svg className="w-5 h-5 text-black font-bold" fill="currentColor" viewBox="0 0 24 24">
+                  {/* <svg className="w-5 h-5 text-black font-bold" fill="currentColor" viewBox="0 0 24 24">
                     <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5" />
-                  </svg>
+                  </svg> */}
+                  <Image src="./logo.svg" alt="Hexific Logo" width={19} height={19} />
                 </div>
                 <span className="text-xl font-bold gradient-text">Hexific</span>
               </div>
