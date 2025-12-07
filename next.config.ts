e4618c1bfe6169556a61e49d7c88c42a2c428638
Febri Nirwana <febri.anjy.nirwana.354@gmail.com>
2025-12-07 16:43:39 +0700

    chore: update next.config.ts to fix vercel

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,20 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack: (config: any, { isServer }: any) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+    };
+    
+    // Exclude test files from bundling
+    config.module.rules.push({
+      test: /\.js$/,
+      include: /node_modules/,
+      exclude: /node_modules\/.*\/test\//,
+      use: ['ignore-loader'],
+    });
 
-const nextConfig: NextConfig= {
-  api: {
-    bodyParser: {
-      sizeLimit: '50mb',
-    },
-  },
-  experimental: {
-    serverComponentsExternalPackages: ['adm-zip'],
+    return config;
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
