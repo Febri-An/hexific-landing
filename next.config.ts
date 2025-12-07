6b1af22a30f9b79677ea83ef87b9e6e52a1eeaf3
Febri Nirwana <febri.anjy.nirwana.354@gmail.com>
2025-12-07 16:50:03 +0700

    chore update next.js config to fix turbopack issue

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config: any, { isServer }: any) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-    };
-    
-    // Exclude test files from bundling
-    config.module.rules.push({
-      test: /\.js$/,
-      include: /node_modules/,
-      exclude: /node_modules\/.*\/test\//,
-      use: ['ignore-loader'],
-    });
-
-    return config;
+  turbopack: {
+    resolveAlias: {
+      // Exclude problematic packages
+      'why-is-node-running': false,
+    },
+  },
+  experimental: {
+    serverComponentsExternalPackages: ['pino', 'pino-pretty'],
   },
 };
 
